d769509400f931879898fd1f0d04bc31dc971611
EsneiderLoaiza <esneiderlvs@gmail.com>
2021-06-29 13:30:09 -0500

    First commit

---
+++ src/main/java/com/example/slot/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.slot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+	
+	@RequestMapping("/re")
+	public String greeting(Model model) {
+		
+		model.addAttribute("name", 1);
+		model.addAttribute("name2", 2);
+		
+		return "greeting_template";
+	}
+
+}
